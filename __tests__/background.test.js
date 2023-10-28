7e379fd0de812d7b17adc850c488554d6df3b111
Strudelhund <61600937+Strudelhund@users.noreply.github.com>
2023-10-28 22:18:42 +0200

    Update background.test.js
    fixed linter findings

--- __tests__/background.test.js
+++ __tests__/background.test.js
@@ -1,19 +1,19 @@
 // Import the fetchMock library
-const fetchMock = require('fetch-mock');
+const fetchMock = require("fetch-mock");
 // Import jsdom to create a virtual DOM environment
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
 
 // Include your main.js file for testing
-const { setRandomDogImageAsBackground } = require('../js/background.js');
+const { setRandomDogImageAsBackground } = require("../js/background.js");
 
 // Mock the fetch request and provide a sample response
-fetchMock.mock('https://dog.ceo/api/breeds/image/random', {
-  status: 'success',
-  message: 'https://images.dog.ceo/breeds/airedale/n02096051_4153.jpg',
+fetchMock.mock("https://dog.ceo/api/breeds/image/random", {
+  status: "success",
+  message: "https://images.dog.ceo/breeds/airedale/n02096051_4153.jpg",
 });
 
 // Create a virtual DOM environment
-const { window } = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+const { window } = new JSDOM("<!DOCTYPE html><html><body></body></html>");
 
 // Assign the window object to the global object
 global.window = window;
@@ -21,15 +21,15 @@ global.document = window.document;
 global.body = window.document.body;
 
 // Define a test case
-test('setRandomDogImageAsBackground sets the background image', async () => {
+test("setRandomDogImageAsBackground sets the background image", async () => {
   // Call the function to be tested
   await setRandomDogImageAsBackground();
 
   // Verify that the background image and styles are set as expected
   const body = document.body;
-  expect(body.style.backgroundImage).toBe('url(https://images.dog.ceo/breeds/airedale/n02096051_4153.jpg)');
-  expect(body.style.backgroundSize).toBe('cover');
-  expect(body.style.backgroundRepeat).toBe('no-repeat');
+  expect(body.style.backgroundImage).toBe("url(https://images.dog.ceo/breeds/airedale/n02096051_4153.jpg)");
+  expect(body.style.backgroundSize).toBe("cover");
+  expect(body.style.backgroundRepeat).toBe("no-repeat");
 });
 
 // Clean up the fetch mock after the test
