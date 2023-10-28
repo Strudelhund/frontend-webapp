63067602222c80af336913d68452c0071fcd420f
Strudelhund <61600937+Strudelhund@users.noreply.github.com>
2023-10-28 22:18:22 +0200

    Update background.js
    fixed linter findings

--- js/background.js
+++ js/background.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Function to set a random dog image as the background
     function setRandomDogImageAsBackground() {
-        fetch('https://dog.ceo/api/breeds/image/random')
+        fetch("https://dog.ceo/api/breeds/image/random")
             .then(response => response.json())
             .then(data => {
-                if (data.status === 'success') {
+                if (data.status === "success") {
                     const imageUrl = data.message;
                     document.body.style.backgroundImage = `url(${imageUrl})`;
-                    document.body.style.backgroundSize = 'cover';
-                    document.body.style.backgroundRepeat = 'no-repeat';
+                    document.body.style.backgroundSize = "cover";
+                    document.body.style.backgroundRepeat = "no-repeat";
                 }
             })
-            .catch(error => console.error('Error fetching dog image:', error));
+            .catch(error => console.error("Error fetching dog image:", error));
     }
 
     // Call the function to set the background image when the page is loaded
